a23efac0527f17076d3850c68c4c9fe42e86ffd8
Charles Francis Brecio <brecio.charlesfrancis@gmail.com>
2026-02-20 12:37:14 +0800

    new ip address for camera

--- modules/CameraServerDiscovery.js
+++ modules/CameraServerDiscovery.js
@@ -5,6 +5,7 @@
  */
 async function tryGetPublicUrl() {
   const localUrls = [
+    'http://10.180.11.156:5000',
     'http://192.168.68.134:5000',
     'http://192.168.1.19:5000',
     'http://10.193.174.156:5000'
@@ -47,14 +48,15 @@ function generateIPsToScan() {
   
   // Common subnet prefixes
   const subnets = [
-    '192.168.68',  // Current network
+    '10.180.11',   // Current network
+    '192.168.68',  // Previous network
     '192.168.1',   // Most common home router
     '192.168.0',   // Second most common
     '192.168.43',  // Android hotspot
     '172.20.10',   // iOS hotspot
     '10.0.0',      // Corporate/Apple
     '10.0.1',      // Corporate variation
-    '10.193.174',  // Previous network
+    '10.193.174',  // Old network
     '172.27.223',  // Mobile carrier
     '192.168.137', // Windows hotspot
   ];
